97190c9cc4fa958c8a5637d19a436f3dcff31512
= <forsvantn@protonmail.com>
2025-10-13 11:32:17 +0200

    feat: implemented least connections

---
+++ routini/src/least_connections.rs
@@ -0,0 +1,165 @@
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
+
+use pingora::{
+    lb::{
+        Backend,
+        selection::{BackendIter, BackendSelection},
+    },
+    protocols::l4::socket::SocketAddr,
+};
+use smallvec::SmallVec;
+
+pub struct LeastConnections {
+    backends: Box<[Backend]>,
+    connections: BTreeMap<SocketAddr, (usize, Arc<AtomicUsize>)>,
+}
+
+impl LeastConnections {
+    pub fn new(backends: &BTreeSet<Backend>) -> Self {
+        let backends = Vec::from_iter(backends.iter().cloned()).into_boxed_slice();
+        Self::from_backends(backends)
+    }
+
+    pub fn from_backends(backends: Box<[Backend]>) -> Self {
+        let connections = backends
+            .iter()
+            .enumerate()
+            .map(|(i, b)| (b.addr.clone(), (i, Arc::new(AtomicUsize::new(0)))))
+            .collect();
+
+        LeastConnections {
+            backends,
+            connections,
+        }
+    }
+}
+
+impl BackendSelection for LeastConnections {
+    type Iter = LeastConnectionsIter;
+
+    fn build(backends: &BTreeSet<Backend>) -> Self {
+        LeastConnections::new(backends)
+    }
+
+    fn iter(self: &Arc<Self>, key: &[u8]) -> Self::Iter {
+        LeastConnectionsIter::new(self.clone(), key)
+    }
+}
+
+pub struct LeastConnectionsIter {
+    least_connections: Arc<LeastConnections>,
+    /// The load balancer should use the first returned backend in the vast majority of cases,
+    /// therefor we use a small vec to track previously returned backends. This lets us skip allocating
+    /// most of the time when the iterator is used.
+    yielded: SmallVec<[usize; 2]>,
+}
+
+impl LeastConnectionsIter {
+    fn new(least_connections: Arc<LeastConnections>, _key: &[u8]) -> Self {
+        Self {
+            least_connections,
+            yielded: SmallVec::new(),
+        }
+    }
+}
+
+impl BackendIter for LeastConnectionsIter {
+    fn next(&mut self) -> Option<&Backend> {
+        let conns = &self.least_connections.connections;
+        let mut min_count = usize::MAX;
+        let mut min_index = None;
+
+        for (i, count) in conns.values() {
+            // skip already yielded backends
+            if self.yielded.contains(i) {
+                continue;
+            }
+            let c = count.load(Ordering::Relaxed);
+            if c < min_count {
+                min_count = c;
+                min_index = Some(*i);
+            }
+        }
+
+        if let Some(i) = min_index {
+            self.yielded.push(i);
+            self.least_connections.backends.get(i)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_connection(least_connections: &Arc<LeastConnections>) -> Option<SocketAddr> {
+        let mut iter = least_connections.iter(&[]);
+        let Some(backend) = iter.next().and_then(|b| Some(b.addr.clone())) else {
+            return None;
+        };
+
+        least_connections
+            .connections
+            .get(&backend)
+            .unwrap()
+            .1
+            .fetch_add(1, Ordering::Relaxed);
+        Some(backend)
+    }
+
+    #[test]
+    fn test_next() {
+        let backend1_addr = SocketAddr::Inet("127.0.0.1:8080".parse().unwrap());
+        let backend2_addr = SocketAddr::Inet("127.0.0.1:8081".parse().unwrap());
+        let backend3_addr = SocketAddr::Inet("127.0.0.1:8082".parse().unwrap());
+
+        let backends = [&backend1_addr, &backend2_addr, &backend3_addr]
+            .iter()
+            .map(|a| Backend::new(&a.to_string()).unwrap())
+            .collect();
+
+        let least_connections = Arc::new(LeastConnections::new(&backends));
+        backends.iter().enumerate().for_each(|(i, b)| {
+            let connections = least_connections.connections.get(&b.addr).unwrap();
+            connections.1.store(i, Ordering::Relaxed);
+        });
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend1_addr.clone()
+        );
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend1_addr.clone()
+        );
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend2_addr.clone()
+        );
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend1_addr.clone()
+        );
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend2_addr.clone()
+        );
+
+        assert_eq!(
+            get_connection(&least_connections).expect("Failed to connect to backend"),
+            backend3_addr.clone()
+        );
+    }
+}
