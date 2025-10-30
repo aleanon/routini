fe20edc24be849692fb6c385812302feb80123d2
= <forsvantn@protonmail.com>
2025-10-30 20:15:22 +0100

    Added new constants and adjusted default values

--- routini/src/utils/constants.rs
+++ routini/src/utils/constants.rs
@@ -1,11 +1,17 @@
+use std::time::Duration;
+
 pub const SET_STRATEGY_ENDPOINT_NAME: &str = "strategy_updater";
 pub const SET_STRATEGY_ENDPOINT_ADDRESS: &str = "0.0.0.0:5000";
-
-pub const DEFAULT_MAX_ALGORITHM_ITERATIONS: usize = 256;
+pub const PROMETHEUS_ENDPOINT_NAME: &str = "prometheus";
+pub const PROMETHEUS_ENDPOINT_ADDRESS: &str = "0.0.0.0:9090";
 
 pub const PATH_REMAINDER_IDENTIFIER: &str = "rest";
 pub const WILDCARD_IDENTIFIER: &str = "{*rest}";
 
-pub const DEFAULT_SMOOTHING_FACTOR: f64 = 0.3;
-pub const DEFAULT_CONNECTIONS_DIV_RATIO: f64 = 0.5;
-pub const DEFAULT_LATENCY_DIV_RATIO: f64 = 0.5;
+pub const DEFAULT_MAX_ALGORITHM_ITERATIONS: usize = 256;
+pub const DEFAULT_SMOOTHING_FACTOR: f32 = 0.5;
+pub const DEFAULT_CONNECTIONS_DIV_RATIO: f32 = 1.2;
+pub const DEFAULT_LATENCY_DIV_RATIO: f32 = 2.0;
+pub const DEFAULT_EVALUATE_STRATEGY_FREQUENCY: Duration = Duration::from_secs(5);
+pub const DEFAULT_HEALTH_CHECK_FREQUENCY: Duration = Duration::from_secs(1);
+pub const DEFAULT_MIN_NR_OF_CONNECTIONS: usize = 1000;
